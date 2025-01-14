cdaa0402d2d060bdf5ee587255c3eb5f3a71ec41
ll <skycreat@gmail.com>
2025-01-14 17:20:26 +0800

    Update default.sh

--- default.sh
+++ default.sh
@@ -2,7 +2,6 @@
 echo '修改 IP'
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
-echo '修改主机名'
 sed -i "s/hostname='OpenWrt'/hostname='Router'/g" package/base-files/files/bin/config_generate
 cat package/base-files/files/bin/config_generate |grep hostname=
 
