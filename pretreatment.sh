61995f6b2a21c0f181056ef9eb6adf33bc180ccb
ll <skycreat@gmail.com>
2025-02-22 09:55:28 +0800

    Update pretreatment.sh

--- pretreatment.sh
+++ pretreatment.sh
@@ -13,13 +13,11 @@ echo '========= K3屏幕驱动插件 加载完成！ ========='
 echo '添加kenzok8 openwrt-package'
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-./scripts/feeds install -a 
-make menuconfig
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang 
 echo '========= kenzok8软件源 加载完成！ ========='
 
 # echo '添加kenzok8 small-package'
