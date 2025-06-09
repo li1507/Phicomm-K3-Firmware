5ce4c5f4a5242580da50a4c5adc7ace0535ff683
ll <skycreat@gmail.com>
2025-06-09 14:08:44 +0800

    Update pretreatment.sh

--- pretreatment.sh
+++ pretreatment.sh
@@ -24,14 +24,14 @@ echo '========= kenzok8软件源 加载完成！ ========='
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # echo '========= kenzok8软件源 加载完成！ ========='
 
-echo '移除bcm53xx中的其他机型'
-sed -i '421,453d' target/linux/bcm53xx/image/Makefile
-sed -i '140,412d' target/linux/bcm53xx/image/Makefile
-sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
-echo '========= 移除bcm53xx中的其他机型完成！ ========='
+# echo '移除bcm53xx中的其他机型'
+# sed -i '421,453d' target/linux/bcm53xx/image/Makefile
+# sed -i '140,412d' target/linux/bcm53xx/image/Makefile
+# sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
+# echo '========= 移除bcm53xx中的其他机型完成！ ========='
 
 # 1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
-firmware='69027'
-echo '替换无线驱动'
-wget -nv https://github.com/li1507/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
-echo '========= 替换无线驱动替换完成！ ========='
+# firmware='69027'
+# echo '替换无线驱动'
+# wget -nv https://github.com/li1507/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+# echo '========= 替换无线驱动替换完成！ ========='
